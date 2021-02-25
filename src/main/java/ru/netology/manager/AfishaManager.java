ca46f4a65d9e9327566d9d4ab7e70425efd9b35e
Nick <>
2021-02-25 22:55:16 +0300

    new version

--- src/main/java/ru/netology/manager/AfishaManager.java
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -2,52 +2,41 @@ package ru.netology.manager;
 
 import lombok.*;
 
-@RequiredArgsConstructor
-@NoArgsConstructor
 @Data
 public class AfishaManager {
 
-    @NonNull
-    int id;
-    @NonNull
-    String name;
-    @NonNull
-    String genre;
-    @NonNull
-    int price;
-    AfishaManager[] items = new AfishaManager[0];
-    int showCount;
+    String[] items = new String[0]; // Массив с фильмами
+    int showCount; // Колчиество выводимого добра
 
     // создание массивва
 
-    public void addToAfishaArray(AfishaManager item) { // создание массива с фильмами
-        int length = items.length+1;
-        AfishaManager[] tmp = new AfishaManager[length];
-        System.arraycopy(items,0,tmp,0,items.length);
-        int lastIndex = tmp.length-1;
-        tmp[lastIndex] = item;
+    public void addToAfishaArray(String id, String name) { // создание массива с фильмами
+        int length = items.length + 1;
+        String[] tmp = new String[length];
+        System.arraycopy(items, 0, tmp, 0, items.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = id + "|" + name;
         items = tmp;
 
     }
 
-    public AfishaManager[] afishaShow(int count){
+    public String[] afishaShow(int count) {
 
-        if (count > items.length || count < 1){
+        if (count > items.length || count < 1) {
             setShowCount(items.length);
-        }
-        else {
+        } else {
             setShowCount(count);
         }
 
 
-        AfishaManager[] result = new AfishaManager[getShowCount()];
-        for (int i = 0; i < getShowCount(); i++){
-            int index = items.length-i-1;
-            result[i] = items [index];
+        String[] result = new String[getShowCount()];
+        for (int i = 0; i < getShowCount(); i++) {
+            int index = items.length - i - 1;
+            result[i] = items[index];
 
         }
 
-        for (int i = 0; i < getShowCount(); i++){
+        for (int i = 0; i < getShowCount(); i++) {
             System.out.println(result[i]);
         }
 
--- src/test/java/ru/netology/manager/AfishaManagerTest.java
+++ src/test/java/ru/netology/manager/AfishaManagerTest.java
@@ -10,35 +10,26 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 class AfishaManagerTest {
 
-    @Test
+    private AfishaManager afisha = new AfishaManager();
 
+    @Test
     public void AddAfishaArrayCount() { // проверяем количество выводимых строк с фильмами
-        AfishaManager afisha = new AfishaManager();
-        AfishaManager item1 = new AfishaManager(1, "King Arthur", "Epic", 99);
-        AfishaManager item2 = new AfishaManager(2, "G.Joe", "Epic", 128);
-        AfishaManager item3 = new AfishaManager(3, "Rope", "Tragedy", 99);
-        AfishaManager item4 = new AfishaManager(4, "Rasamen", "Epic", 99);
-        AfishaManager item5 = new AfishaManager(5, "Hobbit", "Epic", 99);
-        AfishaManager item6 = new AfishaManager(6, "Lord of the Rings", "Epic", 99);
-        AfishaManager item7 = new AfishaManager(7, "Elon Musk", "Epic", 99);
-        AfishaManager item8 = new AfishaManager(8, "Billy", "Epic", 99);
-        AfishaManager item9 = new AfishaManager(9, "Fullmetal", "Epic", 99);
-        AfishaManager item10 = new AfishaManager(10, "Major Pain", "Epic", 99);
-        afisha.addToAfishaArray(item1);
-        afisha.addToAfishaArray(item2);
-        afisha.addToAfishaArray(item3);
-        afisha.addToAfishaArray(item4);
-        afisha.addToAfishaArray(item5);
-        afisha.addToAfishaArray(item6);
-        afisha.addToAfishaArray(item7);
-        afisha.addToAfishaArray(item8);
-        afisha.addToAfishaArray(item9);
-        afisha.addToAfishaArray(item10);
+
+        afisha.addToAfishaArray("1", "King Arthur");
+        afisha.addToAfishaArray("2", "King Arthur2");
+        afisha.addToAfishaArray("3", "Space Odyssey");
+        afisha.addToAfishaArray("4", "Game of Thrones");
+        afisha.addToAfishaArray("5", "Minecraft The Movie");
+        afisha.addToAfishaArray("6", "Mortal Combat");
+        afisha.addToAfishaArray("7", "Vikings");
+        afisha.addToAfishaArray("8", "Winx saga");
+        afisha.addToAfishaArray("9", "SnowPiercer");
+        afisha.addToAfishaArray("10", "Vendetta");
 
         int expected;
 
         expected = 5;
-        AfishaManager[] actual = afisha.afishaShow(5); //проверка длины массива, который выводится. должен соответстовать числу Count
+        String[] actual = afisha.afishaShow(5); //проверка длины массива, который выводится. должен соответстовать числу Count
         Assertions.assertEquals(expected, actual.length);
 
         actual = afisha.afishaShow(0); //Если выводишь 0, то вывести все
