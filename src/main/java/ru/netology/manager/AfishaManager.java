bedb4d0e8ff99130e476974daab236968d549a9d
Nick <>
2021-03-07 21:01:39 +0300

    new

--- src/main/java/ru/netology/manager/AfishaManager.java
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -5,6 +5,7 @@ import ru.netology.domain.CinemaItem;
 
 @Data
 public class AfishaManager {
+    
 
     private AfishaRepository repository;
 
@@ -12,19 +13,36 @@ public class AfishaManager {
         this.repository = repository;
     }
 
+    public AfishaManager() {
+
+    }
+
 
     public void addFilms(CinemaItem item) { // Добавление эелементов
+        System.out.println(item);
         repository.addToAfishaArray(item);
     }
 
     public CinemaItem[] getAll() { // вывод всех фильмов
-        CinemaItem[] items = repository.findall();
-        CinemaItem[] result = new CinemaItem[items.length];
-        for (int i = 0; i < result.length; i++) {
-            int index = items.length - i - 1;
-            result[i] = items[index];
+        CinemaItem[] items = repository.items;
+        items = repository.findall();
+        System.out.println(items);
+        
+        if (items != null){
+            CinemaItem[] result = new CinemaItem[items.length];
+            for (int i = 0; i < result.length; i++) {
+                int index = items.length - i - 1;
+                result[i] = items[index];
+        }
+            return result;
+        }
+        else {
+
+            System.out.println("Где-то случился нуль");
+            return items;
 
         }
-        return result;
+
+        
     }
 }
--- src/test/java/ru/netology/manager/AfishaManagerTest.java
+++ src/test/java/ru/netology/manager/AfishaManagerTest.java
@@ -7,12 +7,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.netology.domain.CinemaItem;
 
+import java.lang.reflect.Array;
+
 @ExtendWith(MockitoExtension.class)
 class AfishaManagerTest {
-    @Mock
+   // @Mock
     private AfishaRepository repository;
-    @InjectMocks
-    private AfishaManager afisha;
+  //  @InjectMocks
+    AfishaManager afisha = new AfishaManager();
     CinemaItem item1 = new CinemaItem(1, "King Arthur",65);
     CinemaItem item2 = new CinemaItem(2, "King Arthur 2",65);
     CinemaItem item3 = new CinemaItem(3, "Game of Thrones",65);
@@ -30,6 +32,8 @@ class AfishaManagerTest {
         CinemaItem[] returned = new CinemaItem[]{item1,item2,item3};
 
         afisha.addFilms(item1);
+
+        Assertions.assertArrayEquals(returned, afisha.getAll());
 
 
 
