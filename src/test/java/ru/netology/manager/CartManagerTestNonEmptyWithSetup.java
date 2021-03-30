54d9fd1bae3b6077b98fed4d01be150db32f7d46
lord6498 <lord6498@gmail.com>
2021-03-30 20:48:51 +0300

    Revert "Cinema manager added + tests"

--- src/test/java/ru/netology/manager/CartManagerTestNonEmptyWithSetup.java
+++ src/test/java/ru/netology/manager/CartManagerTestNonEmptyWithSetup.java
@@ -32,5 +32,15 @@ public class CartManagerTestNonEmptyWithSetup {
     assertArrayEquals(expected, actual);
   }
 
+  @Test
+  public void shouldNotRemoveIfNotExists() {
+    int idToRemove = 4;
 
+    manager.removeById(idToRemove);
+
+    PurchaseItem[] actual = manager.getAll();
+    PurchaseItem[] expected = new PurchaseItem[]{third, second, first};
+
+    assertArrayEquals(expected, actual);
+  }
 }
